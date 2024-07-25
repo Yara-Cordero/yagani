09c326860c317d4b912299e33d1f81001e47507e
Yara-Cordero <145566014+Yara-Cordero@users.noreply.github.com>
2024-07-25 14:07:37 +0200

    create race result crawler

--- my-f1crawler/src/main.ts
+++ my-f1crawler/src/main.ts
@@ -3,13 +3,18 @@ import { PlaywrightCrawler, ProxyConfiguration } from 'crawlee';
 
 import { router } from './routes.js';
 
-const startUrls = ['https://crawlee.dev'];
+const startUrls = ['https://www.formula1.com/en/results/'];
 
 const crawler = new PlaywrightCrawler({
+    launchContext: {
+        launchOptions: {
+            headless: true,
+        },
+    },
     // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
     requestHandler: router,
     // Comment this option to scrape the full website.
-    maxRequestsPerCrawl: 20,
+    maxRequestsPerCrawl: 74,
 });
 
 await crawler.run(startUrls);
--- my-f1crawler/src/routes.ts
+++ my-f1crawler/src/routes.ts
@@ -1,21 +1,46 @@
 import { createPlaywrightRouter } from 'crawlee';
+import {selectors} from "playwright";
 
 export const router = createPlaywrightRouter();
+
+const rowselector = 'tbody > tr';
+const headselector = 'thead > tr > th';
 
 router.addDefaultHandler(async ({ enqueueLinks, log }) => {
     log.info(`enqueueing new URLs`);
     await enqueueLinks({
-        globs: ['https://crawlee.dev/**'],
+        globs: ['https://www.formula1.com/en/results/**'],
         label: 'detail',
     });
 });
 
 router.addHandler('detail', async ({ request, page, log, pushData }) => {
     const title = await page.title();
-    log.info(`${title}`, { url: request.loadedUrl });
 
+   const headerData = await page.$$eval(headselector, headers => {
+       return headers.map(header => header.innerText.trim());
+   })
+
+   const tableData = await page.$$eval(rowselector, (rows, headers) => {
+       return rows.map(row => {
+           const cells = row.querySelectorAll('td');
+           const cellData = Array.from(cells).map(cell => cell.innerText.trim())
+           const rowData = {};
+           headers.forEach((col, index) => {
+               rowData[col] = cellData[index] || '';
+           });
+           return rowData;
+       });
+   }, headerData) ;
+
+
+
+
+
+    log.info(`${title}`, { url: request.loadedUrl });
     await pushData({
         url: request.loadedUrl,
         title,
+        tableData
     });
 });
